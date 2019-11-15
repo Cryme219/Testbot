7b14f5b0a595dcaf48c3cfaa32a65e62d26f790d
Cryme219 <57401752+Cryme219@users.noreply.github.com>
2019-11-15 20:57:26 +0400

    Add files via upload

---
+++ Test bot/Test.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+client = commands.Bot(command_prefix = "*")
+
+@client.event
+async def on_ready():
+    print("Online!")
+
+@client.command()
+async def role(ctx, *, role: discord.Role):
+    roles = discord.utils.get(ctx.guild.roles, name = f'{role}')
+    author = ctx.message.author 
+    await author.add_roles(roles)
+    await author.send(f'{role} has been added.')
+
+@client.command()
+async def createarena(ctx, arenaID, Pass: int):
+    author = ctx.message.author
+    guild = ctx.message.guild
+    if Pass >  100000000:
+        msg = await ctx.channel.send("Arena password is invalid")
+        await asyncio.sleep(10)
+        await msg.delete()
+    else:
+         channel = await guild.create_text_channel(f'{author}s arena')
+         await channel.send(f'__**{author} arena**__\nID = {arenaID} \nPassword = {Pass}')
+    
+    
+
+
+
+
+
+
+
+@client.command()
+async def unrole(ctx, *, role: discord.Role):
+    roles = discord.utils.get(ctx.guild.roles, name = f'{role}')
+    author = ctx.message.author
+    await author.remove_roles(roles)
+    await author.send(f'{role} has been successfully removed')
+client.run("NjQ0NzY5MTkxMzY2NDkyMTY0.Xc42Nw.mm8c1_kPnon_D1GMfb-8O2MuplY")
+    
